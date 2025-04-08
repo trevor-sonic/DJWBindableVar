28ad033c8f0643b5527f3bd354e7874fe3fcbbf8
Trevor <trevor.d@beydag.com>
2025-04-08 02:49:28 +0100

    Auto update.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .iOS(.v13),
         .macOS(.v10_12),
         .tvOS(.v10)
-    ],
+    ], 
     
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
